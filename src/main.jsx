a8a96e0fb85a2944714fae6f26bf828ad166f6d3
Mahak Patel <mahakpateluiux@gmail.com>
2026-08-16 03:00:09 -0400

    fix: load final mobile home first impression polish

--- src/main.jsx
+++ src/main.jsx
@@ -38,6 +38,7 @@ import './mobile-image-safety-final.css'
 import './mobile-alignment-final.css'
 import './mobile-device-universal.css'
 import './mobile-home-meta-border-fix.css'
+import './mobile-home-first-impression-final.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
