6689a58df3b163a3ed2ba49b82a573539e646227
Mahak Patel <mahakpateluiux@gmail.com>
2026-08-15 22:45:28 -0400

    feat: load enhanced gallery styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './styles.css'
 import './enhancements.css'
 import './brand.css'
 import './restaurant-polish.css'
+import './gallery-enhanced.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
